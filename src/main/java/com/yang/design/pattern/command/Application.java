060f9c4e0b2982a179df9504a582b26022cd2608
544759698 <544759698@qq.com>
2018-07-29 23:50:56 +0800

    command

---
+++ src/main/java/com/yang/design/pattern/command/Application.java
@@ -0,0 +1,7 @@
+package com.yang.design.pattern.command;
+
+/**
+ * Created by Administrator on 2018/7/29.
+ */
+public class Application {
+}
---
+++ src/main/java/com/yang/design/pattern/observe/Application.java
@@ -0,0 +1,7 @@
+package com.yang.design.pattern.observe;
+
+/**
+ * Created by Administrator on 2018/7/29.
+ */
+public class Application {
+}
